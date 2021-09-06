4e123b6b2f243aa4c9e6880254f3441ce590e091
IkumaTadokoro <tadokorodev@gmail.com>
2021-09-06 11:44:59 +0900

    初回コミット

---
+++ index.js
@@ -0,0 +1,27 @@
+const program = require('commander')
+const setToken = require('./lib/set-token')
+const createPlaylist = require('./lib/create-playlist')
+
+const onFatalError = () => {
+  process.exitCode = 2
+  console.error('Oops! Something went wrong! :(')
+}
+
+const main = () => {
+  process.on('uncaughtException', onFatalError)
+  process.on('unhandledRejection', onFatalError)
+
+  program
+    .command('createPlaylist', { isDefault: true })
+    .description('<Default> Create private playlist for your spotify account. Note:Require \'suggestify settoken\' command before the first time you run.')
+    .action(() => createPlaylist())
+
+  program
+    .command('settoken')
+    .description('Login Spotify via authorization code flow (Refer: https://developer.spotify.com/documentation/general/guides/authorization-guide/).')
+    .action(() => setToken())
+
+  program.parse()
+}
+
+main()
